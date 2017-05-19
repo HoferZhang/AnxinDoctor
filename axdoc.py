e3c2130ec2ce25850a421ceb5247d0d9f18aaf79
HoferZhang <Zhang.hofer@gmail.com>
2017-05-19 18:51:17 +0800

    login, but sendkeys fail

--- axdoc.py
+++ axdoc.py
@@ -5,6 +5,7 @@ import unittest
 import common
 import config
 import time
+import logging
 
 from appium import webdriver
 
@@ -15,8 +16,12 @@ class AxDoc(unittest.TestCase):
             'platformName': config.CONNECT['platformName'],
             'platformVersion': config.CONNECT['platformVersion'],
             'deviceName': config.CONNECT['deviceName'],
+            'udid': config.CONNECT['udid'],
             'appPackage': config.CONNECT['appPackage'],
-            'appActivity': config.CONNECT['appActivity']
+            'appActivity': config.CONNECT['appActivity'],
+            'unicodeKeyboard': config.CONNECT['unicodeKeyboard'],
+            'deviceReadyTimeout': config.CONNECT['deviceReadyTimeout'],
+            'resetKeyboard': config.CONNECT['resetKeyboard']
         }
         self.driver = webdriver.Remote(config.CONNECT['baseUrl'], desired_caps)
 
@@ -28,11 +33,25 @@ class AxDoc(unittest.TestCase):
     def test_log_in(self):
         # 确认进入
         self.driver.implicitly_wait(10)
-        self.driver.find_element_by_id('com.hilficom.anxindoctor:id/login_tv').click()
+        go_to = self.driver.find_element_by_id('com.hilficom.anxindoctor:id/login_tv')
+        # self.assertEqual('立即进入',go_to.text)
+        go_to.click()
 
         # 登录
+        self.driver.implicitly_wait(10)
+        mobile = self.driver.find_element_by_xpath(
+            '//android.widget.EditText[@resource-id=\"com.hilficom.anxindoctor:id/edit_text_et\" and @text=\"手机\"]'
+        )
+        mobile.click()
+        mobile.clear()
+        mobile.send_keys(u"18500385003")
 
-        self.driver.find_element_by_xpath('//android.widget.LinearLayout[@resource-id=\"com.hilficom.anxindoctor:id/password_ll\"]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.EditText[1]').click()
+        self.driver.implicitly_wait(10)
+        pwd = self.driver.find_element_by_xpath(
+            '//android.widget.LinearLayout[@resource-id=\"com.hilficom.anxindoctor:id/password_ll'
+            '\"]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.EditText[1]'
+        )
+        pwd.send_keys("111111")
         time.sleep(5)
 
 
--- config.py
+++ config.py
@@ -4,8 +4,12 @@
 CONNECT = {
     'platformName': 'Android',
     'platformVersion': '5.1',
-    'deviceName': '85UABM8USH8G',
+    'deviceName': 'a Android device',
+    'udid': '85UABM8USH8G',
     'appPackage': 'com.hilficom.anxindoctor',
     'appActivity': 'com.hilficom.anxindoctor.activity.basic.SplashActivity',
-    "baseUrl": "http://127.0.0.1:4723/wd/hub"
+    'unicodeKeyboard': 'true',
+    'resetKeyboard': 'true',
+    "deviceReadyTimeout": '5000',
+    "baseUrl": "http://127.0.0.1:4723/wd/hub",
 }
